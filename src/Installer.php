ce489c73e041d68e7462ca8f860f0300e6464be3
Bob Moore <bobjutsu@gmail.com>
2023-12-13 20:19:00 -0600

    Testing new install script

--- src/Installer.php
+++ src/Installer.php
@@ -2,10 +2,15 @@
 
 namespace Devkit\PluginBuilder;
 
+require_once dirname( __DIR__, 1 ) . '/vendor/autoload.php';
+
+use Composer\Script\Event;
+
 class Installer
 {
 
-    public static function install( $event ) {
+    public static function install( Event $event )
+    {
         var_dump( get_class($event) );
         $io = $event->getIO();
         $user = $io->ask( 'Enter your Username: ');
