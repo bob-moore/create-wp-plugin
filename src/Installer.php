0eeeeaececaa4473ebd0f11a366b66fb878f0b13
Bob Moore <bobjutsu@gmail.com>
2023-12-13 20:13:33 -0600

    Testing new install script

--- src/Installer.php
+++ src/Installer.php
@@ -5,7 +5,10 @@ namespace Devkit\PluginBuilder;
 class Installer
 {
 
-    public static function install( $c ) {
-        var_dump( get_class_methods($c) );
+    public static function install( $event ) {
+        var_dump( get_class($event) );
+        $io = $event->getIO();
+        $user = $io->ask( 'Enter your Username: ');
+        echo $user;
     }
 }
