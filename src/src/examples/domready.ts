1e980737197b74f7726444da8ce6049cd571b84b
Bob Moore <bobjutsu@gmail.com>
2023-12-27 11:05:27 -0600

    Added jquery and domready examples to src

---
+++ src/src/examples/domready.ts
@@ -0,0 +1,5 @@
+import domReady from '@wordpress/dom-ready';
+
+domReady( () => {
+    // Do something when the DOM is ready.
+} );
---
+++ src/src/examples/jquery.ts
@@ -0,0 +1,8 @@
+import jQuery from 'jquery';
+
+jQuery( function ( $: JQueryStatic ) {
+	'use strict';
+    /**
+     * Do something with jQuery, '$' is aliased to 'jQuery'
+     */
+} );
